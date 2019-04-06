042aaa414f073616f1bca2daf96db796bf674daf
adnesh <dhamangaonadnesh@gmail.com>
2019-04-06 23:01:26 +0530

    seperate file created 'gameinfo' containing some important variables

---
+++ gameinfo.py
@@ -0,0 +1,15 @@
+import sdl2.ext as sdl
+
+WINDOW_WIDTH = 700
+WINDOW_HEIGHT = 900
+GAP = 80
+COLOR_GRID =     {   "black":sdl.Color(0, 0, 0, 0), 
+                     "white":sdl.Color(255, 255, 255, 0), 
+                     "red":sdl.Color(255, 0, 0, 0), 
+                     "green":sdl.Color(0, 255, 0, 0),
+                     "white-green":sdl.Color(190, 255, 190, 0),
+                     "blue":sdl.Color(0, 0, 255, 0),
+                     "blue-green":sdl.Color(0, 255, 255, 0)
+                 }
+
+
