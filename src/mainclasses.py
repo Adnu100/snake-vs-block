1255c7ba4a629a7b064e23374e4ac0a6f848e8cf
Adnu100 <dhamangaonadnesh@gmail.com>
2019-06-05 18:46:01 +0530

    game now returns exit status

--- src/mainclasses.py
+++ src/mainclasses.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import gameinfo
 import objects_to_display as ob
 import sdl2
@@ -17,7 +15,6 @@ class Gamewindow(sdl.Renderer):
 
     def __init__(self):
         self.w = sdl.Window("Adnesh's Snake vs Block Game", (gameinfo.WINDOW_WIDTH, gameinfo.WINDOW_HEIGHT))
-        self.i = 0
         sdl.Renderer.__init__(self, self.w)
         self.mode = gameinfo.BLOCK_IN_MOTION
         self.t = ttf.TTF_OpenFont(b"../support/font.ttf", 30)
@@ -104,7 +101,7 @@ class Maingame:
         self.rows = ob.BlockRows()
         self.g = []
 
-    def Start(self):
+    def Start(self, ischeck):
         self.r.w.show()
         Running = True
         i = 0
@@ -144,19 +141,19 @@ class Maingame:
             if self.rows.row:
                 if self.rows.row[0].pos > lim:
                     self.rows.mountrow(self.snake)
-                    lim = random.randint(gameinfo.BLOCKSIZE, 900) #+ gameinfo.BLOCKSIZE + gameinfo.BONUSRADIUS
+                    lim = random.randint(gameinfo.BLOCKSIZE, 900) 
                 random.seed(random.random())
                 if random.randint(1, 500) == random.randint(1, 500) and self.rows.row[0].pos > gameinfo.BLOCKSIZE:
                     goody = ob.Goody(random.randint(1, 3))
                     self.g.append(goody)
             else:
                 self.rows.mountrow(self.snake)
-            if len(sys.argv) > 1 and sys.argv[1] == "-check":
+            #if len(sys.argv) > 1 and "--check" in sys.argv:
+            if ischeck:
                 if self.snake.l == 1:
                     self.snake.collect(6)
             if self.snake.head != None:
                 for h in self.g:
-                    #if h.pos != self.snake.head[1]:
                     if h.pos > (self.snake.head[1] + self.snake.RADIUS) or h.pos < (self.snake.head[1] - self.snake.RADIUS):
                         continue
                     for c in range(len(h.co)):
@@ -211,18 +208,27 @@ class Maingame:
         f.close()
 
 def StartGame():
+    # handling command line arguments with an unstandard way 
+    # because there are may few options and they do not
+    # affect time complexity
+    ischeck = False
+    if len(sys.argv) > 1:
+        if "--check" in sys.argv:
+            ischeck = True
+        else:
+            ischeck = False
+        if "--reset" in sys.argv:
+            ResetScore()
+            return 0
+        if "--score" in sys.argv:
+            DisplayHighscore()
+            return 0
     sdl2.SDL_Init(sdl2.SDL_INIT_EVERYTHING)
     sdl.init()
     ttf.TTF_Init()
-    Maingame().Start()
+    Maingame().Start(ischeck)
     sdl.quit()
     ttf.TTF_Quit()
     sdl2.SDL_Quit()
-
-if __name__ == '__main__':
-    StartGame()
-    #i = Maingame()
-    #i.Start()
-
-            
+    return 0
 
---
+++ src/start.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import mainclasses
+
+if __name__ == '__main__':
+    mainclasses.sys.exit(mainclasses.StartGame())
